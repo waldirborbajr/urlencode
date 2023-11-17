9d6b7d185fa3b3e2850fcfa4b36021df4b717325
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-11-16 21:44:29 -0300

    Rand Message: /spotless-color

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,20 @@
-use clap::{App, Arg};
-use std::io::stdin;
-use urlencoding::{decode, encode};
+use urlencoding::encode;
 
 fn main() {
-    let decode_arg = Arg::new("decode")
-        .short('d')
-        .long("decode")
-        .help("decode data");
+    let mut param = std::env::args().skip(1);
 
-    let file_arg = Arg::new("FILE").index(1).required(false);
+    let message = if let Some(content) = param.next() {
+        match content.trim() {
+            "" => {
+                eprintln!("Please inform non-empty content");
+                String::new()
+            }
+            trimmed_content => trimmed_content.to_string(),
+        }
+    } else {
+        eprintln!("Please inform the content");
+        String::new()
+    };
 
-    let args = App::new("urlencode")
-        .about("")
-        .arg(decode_arg)
-        .arg(file_arg)
-        .get_matches();
-
-    let mut input = String::new();
-    stdin().read_line(&mut input).expect("No input provided");
-
-    if args.is_present("decode") {
-        println!("{}", decode(&input.trim_end()).unwrap());
-        return;
-    }
-    println!("{}", encode(&input.trim_end()));
+    println!("{}", encode(&message));
 }
